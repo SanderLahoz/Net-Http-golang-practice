350bda98aad3194800845e3f0c9a04ae0f1ef429
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-06-19 21:20:36 +0200

    Simple loadbalancer implementation

---
+++ loadbalancer/loadbalancer.go
@@ -0,0 +1,68 @@
+package loadbalancer
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strconv"
+)
+
+var (
+	baseUrl = "http://localhost:808"
+)
+
+type LoadBalancer struct {
+	RevProxy httputil.ReverseProxy
+}
+
+type Endpoints struct {
+	List []*url.URL
+}
+
+// Shuffle : Shuffles through the list like a cartwheel
+func (e *Endpoints) Shuffle() {
+	temp := e.List[0]
+	e.List = e.List[1:]
+	e.List = append(e.List, temp)
+}
+
+func MakeLoadBalancer(amount int) {
+	var lb LoadBalancer
+	var ep Endpoints
+
+	// Server + Router
+	router := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8090",
+		Handler: router,
+	}
+
+	// Creating the endpoints
+	for i := 0; i < amount; i++ {
+		ep.List = append(ep.List, createEndpoint(baseUrl, i))
+	}
+
+	// Handler functions
+	router.HandleFunc("/loadbalancer", makeRequest(&lb, &ep))
+
+	// Listen and server
+	log.Fatal(server.ListenAndServe())
+}
+
+func makeRequest(lb *LoadBalancer, ep *Endpoints) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		lb.RevProxy = *httputil.NewSingleHostReverseProxy(ep.List[0])
+		lb.RevProxy.ServeHTTP(w, r)
+		ep.Shuffle()
+	}
+}
+
+func createEndpoint(endpoint string, idx int) *url.URL {
+	link := endpoint + strconv.Itoa(idx)
+	parsedUrl, err := url.Parse(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return parsedUrl
+}
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import (
-	"loadbalancer/servers"
-)
+import "loadbalancer/loadbalancer"
+
+// First uncomment the servers.RunServers to start 5 server instances
+// Then uncomment the loadbalancer.MakeLoadBalancer and comment out the
+// servers.RunServers and run the file in a new terminal to start the
+// loadbalancer then visit the loadbalancer path localhost:8090/loadbalancer
 
 func main() {
-	servers.RunServers(5)
+	// servers.RunServers(5)
+	loadbalancer.MakeLoadBalancer(5)
 }
