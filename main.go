6765caca9983ac9d1b7dec456a990a0853f10a63
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2026-06-19 19:21:18 +0200

    Initial server creation

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import (
+	"loadbalancer/servers"
+)
 
+func main() {
+	servers.Test()
 }
--- servers/servers.go
+++ servers/servers.go
@@ -1,1 +1,37 @@
 package servers
+
+import (
+	"fmt"
+	"log"
+)
+
+type ServerList struct {
+	Ports []int
+}
+
+func (s *ServerList) Populate(amount int) {
+	if amount > 10 {
+		log.Fatalf("Amount of ports cant exceed 10")
+	}
+
+	for i := 0; i < amount; i++ {
+		s.Ports = append(s.Ports, i)
+	}
+}
+
+func (s *ServerList) Pop() int {
+	port := s.Ports[0]
+	s.Ports = s.Ports[1:]
+	return port
+}
+
+func Test() {
+	var myServerList ServerList
+
+	myServerList.Populate(5)
+	limit := len(myServerList.Ports)
+
+	for i := 0; i < limit; i++ {
+		fmt.Println(myServerList.Pop())
+	}
+}
